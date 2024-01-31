20b2633cd005d2c51e90829e4c415fc4e4692733
oliverescobar <103139909+oliverescobar@users.noreply.github.com>
2024-01-31 10:41:37 -0700

    this is a fix to the printboard

--- TicTacToeTools.cs
+++ TicTacToeTools.cs
@@ -10,13 +10,20 @@ namespace Mission4_Main_Program
     {
         public void PrintBoard(string[] gameBoardInfo)
         {
-
+            aInput = gameBoardInfo;
+            // This is the board output with numbers assigned!
+            Console.WriteLine($"[ {aInput[0]} ] | [ {aInput[1]} ] | [ {aInput[2]} ] ");
+            Console.WriteLine($"[ {aInput[3]} ] | [ {aInput[4]} ] | [ {aInput[5]} ] ");
+            Console.WriteLine($"[ {aInput[6]} ] | [ {aInput[7]} ] | [ {aInput[8]} ] ");
         }
         //Print the board in this method
         //Don't return anything
         //Input will look like this [ , ,X, , ,O, , ,O]
 
         public string[] Winner(string gameBoardInfo)
+        {
+
+        }
         //[true, X's]
         //Input will look like this [ , ,X, , ,O, , ,O] 
         //Return string "X" "O" "T" "-" 
