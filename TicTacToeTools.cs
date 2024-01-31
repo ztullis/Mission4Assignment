57c0171fd558ba073f6c927bb5da4986a6a640f5
Zach Tullis <zachjtullis@gmail.com>
2024-01-31 08:51:25 -0700

    Created notes of how we want to organize the classes

--- TicTacToeTools.cs
+++ TicTacToeTools.cs
@@ -11,8 +11,11 @@ namespace Mission4_Main_Program
         public void PrintBoard(string[] gameBoardInfo)
         //Print the board in this method
         //Don't return anything
+        //Input will look like this [ , ,X, , ,O, , ,O]
 
         public string[] Winner(string gameBoardInfo)
         //[true, X's]
+        //Input will look like this [ , ,X, , ,O, , ,O] 
+        //Return string "X" "O" "T" "-" 
     }
 }
