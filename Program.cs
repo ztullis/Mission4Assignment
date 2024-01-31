b826f22bfbca61d8deb370e422293f5c4698df3f
Zach Tullis <zachjtullis@gmail.com>
2024-01-31 08:43:58 -0700

    Deleted classes from main program

--- Program.cs
+++ Program.cs
@@ -1,9 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-
-public void PrintBoard(string[] gameBoardInfo)
-    //Print the board in this method
-    //Don't return anything
-
-public string[] Winner(string gameBoardInfo)
-    //[true, X's]
