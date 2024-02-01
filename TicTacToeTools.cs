12f7a69ef2ca2db57eebf97a3e52d6064606485a
joshuagthatcher <racemts@byu.edu>
2024-02-01 09:24:17 -0700

    Joshua CheckWinner

--- TicTacToeTools.cs
+++ TicTacToeTools.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Mission4_Main_Program
 {
+
     internal class TicTacToeTools
     {
         public void PrintBoard(string[] gameBoardInfo)
@@ -16,9 +18,103 @@ namespace Mission4_Main_Program
         //Don't return anything
         //Input will look like this [ , ,X, , ,O, , ,O]
 
-        public string[] Winner(string gameBoardInfo)
+        public string Winner(string gameBoardInfo)
+
+        {
+            string winner = "";
+
+            for (int iCount = 0; iCount < gameBoardInfo.Length; iCount++)
+            {
+                if (gameBoardInfo[0] == 'X' && gameBoardInfo[1] == 'X' && gameBoardInfo[2] == 'X')
+                {
+                    winner = "X";
+                }
+                else if (gameBoardInfo[3] == 'X' && gameBoardInfo[4] == 'X' && gameBoardInfo[5] == 'X')
+                {
+                    winner = "X";
+                }
+                else if (gameBoardInfo[6] == 'X' && gameBoardInfo[7] == 'X' && gameBoardInfo[8] == 'X')
+                {
+                    winner = "X";
+                }
+                else if (gameBoardInfo[0] == 'X' && gameBoardInfo[3] == 'X' && gameBoardInfo[6] == 'X')
+                {
+                    winner = "X";
+                }
+                else if (gameBoardInfo[1] == 'X' && gameBoardInfo[4] == 'X' && gameBoardInfo[7] == 'X')
+                {
+                    winner = "X";
+                }
+                else if (gameBoardInfo[2] == 'X' && gameBoardInfo[5] == 'X' && gameBoardInfo[8] == 'X')
+                {
+                    winner = "X";
+                }
+                else if (gameBoardInfo[0] == 'X' && gameBoardInfo[4] == 'X' && gameBoardInfo[8] == 'X')
+                {
+                    winner = "X";
+                }
+                else if (gameBoardInfo[2] == 'X' && gameBoardInfo[4] == 'X' && gameBoardInfo[6] == 'X')
+                {
+                    winner = "X";
+                }
+                if (gameBoardInfo[0] == 'O' && gameBoardInfo[1] == 'O' && gameBoardInfo[2] == 'O')
+                {
+                    winner = "X";
+                }
+                else if (gameBoardInfo[3] == 'O' && gameBoardInfo[4] == 'O' && gameBoardInfo[5] == 'O')
+                {
+                    winner = "X";
+                }
+                else if (gameBoardInfo[6] == 'O' && gameBoardInfo[7] == 'O' && gameBoardInfo[8] == 'O')
+                {
+                    winner = "O";
+                }
+                else if (gameBoardInfo[0] == 'O' && gameBoardInfo[3] == 'O' && gameBoardInfo[6] == 'O')
+                {
+                    winner = "O";
+                }
+                else if (gameBoardInfo[1] == 'O' && gameBoardInfo[4] == 'O' && gameBoardInfo[7] == 'O')
+                {
+                    winner = "O";
+                }
+                else if (gameBoardInfo[2] == 'O' && gameBoardInfo[5] == 'O' && gameBoardInfo[8] == 'O')
+                {
+                    winner = "O";
+                }
+                else if (gameBoardInfo[0] == 'O' && gameBoardInfo[4] == 'O' && gameBoardInfo[8] == 'O')
+                {
+                    winner = "O";
+                }
+                else if (gameBoardInfo[2] == 'O' && gameBoardInfo[4] == 'O' && gameBoardInfo[6] == 'O')
+                {
+                    winner = "O";
+                }
+                else if (
+                    ((gameBoardInfo[0] == 'X') || (gameBoardInfo[0] == 'Y')) &&
+                    ((gameBoardInfo[1] == 'X') || (gameBoardInfo[1] == 'Y')) &&
+                    ((gameBoardInfo[2] == 'X') || (gameBoardInfo[2] == 'Y')) &&
+                    ((gameBoardInfo[3] == 'X') || (gameBoardInfo[3] == 'Y')) &&
+                    ((gameBoardInfo[4] == 'X') || (gameBoardInfo[4] == 'Y')) &&
+                    ((gameBoardInfo[5] == 'X') || (gameBoardInfo[5] == 'Y')) &&
+                    ((gameBoardInfo[6] == 'X') || (gameBoardInfo[6] == 'Y')) &&
+                    ((gameBoardInfo[7] == 'X') || (gameBoardInfo[7] == 'Y')) &&
+                    ((gameBoardInfo[8] == 'X') || (gameBoardInfo[8] == 'Y'))
+                )
+                {
+                    winner = "T";
+                }
+
+                else
+                {
+                    winner = "-";
+                }
+                return winner;
+            }
+        }
+    }
+}
         //[true, X's]
         //Input will look like this [ , ,X, , ,O, , ,O] 
         //Return string "X" "O" "T" "-" 
-    }
-}
+
+
